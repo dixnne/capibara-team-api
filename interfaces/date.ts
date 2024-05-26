bb779330d9bd75902d12dfc90e2f4b4406297a11
Diana Narvaez <dianapa.narvaez@gmail.com>
2024-05-26 16:14:03 -0600

    Add REST API endpoints to app.js

--- interfaces/date.ts
+++ interfaces/date.ts
@@ -1,11 +1,13 @@
 export interface DateInfo {
     id: number,
-    petId: number,
-    date: {
-        day: number,
-        month: number,
-        year: number,
-        hour: string
-    },
-    userId: number
+    data: {
+        petId: number,
+        date: {
+            day: number,
+            month: number,
+            year: number,
+            hour: string
+        },
+        userId: number
+    }
 }
