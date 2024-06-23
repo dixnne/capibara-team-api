c19a7128991f74763e3805f9acfad47a8a0549df
Diana Narvaez <dianapa.narvaez@gmail.com>
2024-06-22 22:56:00 -0600

    Add cors pkg

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
 import router from "./routes/routes.js";
+import cors from "cors";
 
 const app = express();
 const port = 4000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/", router);
